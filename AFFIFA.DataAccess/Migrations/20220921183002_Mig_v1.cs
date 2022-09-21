0b3c1de282f76613be2bf44642e0c5bacd890a91
Razumickin <razumickin@hotmail.com>
2022-09-21 15:33:37 -0300

    feat: Entity Jogador criada. Migration v1 criada.

---
+++ AFFIFA.DataAccess/Migrations/20220921183002_Mig_v1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AFFIFA.DataAccess.Migrations
+{
+    public partial class Mig_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jogadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCompleto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NomeCurto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SofifaId = table.Column<int>(type: "int", nullable: false),
+                    EquipeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogadores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jogadores_Equipes_EquipeId",
+                        column: x => x.EquipeId,
+                        principalTable: "Equipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogadores_EquipeId",
+                table: "Jogadores",
+                column: "EquipeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogadores");
+        }
+    }
+}
---
+++ AFFIFA.Domain/Entities/Jogador.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AFFIFA.Domain.Entities
+{
+    public class Jogador
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; } = default!;
+        public string NomeCurto { get; set; } = default!;
+        public int SofifaId { get; set; } = default!;
+        public Equipe? Equipe { get; set; }
+    }
+}
